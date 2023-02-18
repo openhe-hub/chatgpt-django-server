5ad797aa6c49750adef634f6f530fd5dcef95bb5
openeh <625529334@qq.com>
2023-02-18 20:17:26 +0800

    2-18-upd: remove sqlite3 module

--- src/chat_server/chat_server/settings.py
+++ src/chat_server/chat_server/settings.py
@@ -73,12 +73,12 @@ WSGI_APPLICATION = 'chat_server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
