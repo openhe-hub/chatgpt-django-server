3374e309ec79bfcbac2de1aedebf8b33fcac8b8a
openeh <625529334@qq.com>
2023-02-18 19:59:48 +0800

    2-18-upd: modify default port

--- src/chat_server/manage.py
+++ src/chat_server/manage.py
@@ -2,6 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from django.core.management.commands.runserver import Command as Runserver
 
 
 def main():
@@ -19,4 +20,5 @@ def main():
 
 
 if __name__ == '__main__':
+    Runserver.default_port = 9000
     main()
